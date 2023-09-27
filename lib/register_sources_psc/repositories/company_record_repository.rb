52acf8a2fad6ea5c6d3d6e639d455de66e4d90cb
Nicolas Williams <tp@tiredpixel.com>
2023-09-27 16:18:51 +0200

    [#188] note Repositories::CompanyRecordRepository process_results fixme
    This class has `process_results` method which uses `sort` with a single
    parameter, despite it requiring at least two parameters. I think that
    this is a mistake, but I'm disabling RuboCop, as attempting to change it
    elsewhere led to test failures.

--- lib/register_sources_psc/repositories/company_record_repository.rb
+++ lib/register_sources_psc/repositories/company_record_repository.rb
@@ -204,7 +204,7 @@ module RegisterSourcesPsc
 
       def process_results(results)
         hits = results.dig('hits', 'hits') || []
-        hits = hits.sort { |hit| hit['_score'] }.reverse
+        hits = hits.sort { |hit| hit['_score'] }.reverse # rubocop:disable Lint/UnexpectedBlockArity # FIXME
 
         mapped = hits.map do |hit|
           SearchResult.new(map_es_record(hit['_source']), hit['_score'])
