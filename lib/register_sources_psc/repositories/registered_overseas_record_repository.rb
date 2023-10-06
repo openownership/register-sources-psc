openownership/register-sources-psc

commit 67b32260f736b2768e51a6bbe8481422473ec00b

[#188] fix Repositories::RegisteredOverseasRecordRepository to call super
This has an identical `initialize` method to its parent, but with
different defaults in the signature. Rather than duplicate the method
and never call `super`, call `super` passing the variables.

lib/register_sources_psc/repositories/registered_overseas_record_repository.rb
@@ -9,8 +9,7 @@ module RegisterSourcesPsc
   module Repositories
     class RegisteredOverseasRecordRepository < CompanyRecordRepository
       def initialize(client: Config::ELASTICSEARCH_CLIENT, index: Config::ES_OVERSEAS_RECORD_INDEX)
-        @client = client
-        @index = index
+        super(client, index)
       end
     end
   end

commit 6a40fe38200bc2d1e7a4b7193dd1bc38197fb647

[#188] fix Repositories::RegisteredOverseasRecordRepository initialize
This was broken in 67b32260f736b2768e51a6bbe8481422473ec00b during the
refactor.

lib/register_sources_psc/repositories/registered_overseas_record_repository.rb
@@ -8,7 +8,7 @@ module RegisterSourcesPsc
   module Repositories
     class RegisteredOverseasRecordRepository < CompanyRecordRepository
       def initialize(client: Config::ELASTICSEARCH_CLIENT, index: Config::ELASTICSEARCH_INDEX_OVERSEAS)
-        super(client, index)
+        super(client:, index:)
       end
     end
   end